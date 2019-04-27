2d044c086d08dffd5944694c8b729bc33eddb183
Wes Nicol <38409659+wesnicol@users.noreply.github.com>
2019-04-26 22:33:37 -0500

    Update create_theaters_dictionary.py

--- create_theaters_dictionary.py
+++ create_theaters_dictionary.py
@@ -9,6 +9,9 @@ theater_list = get_theaters()
 
 for theaters in theater_list:
 
+    # the next thing you need to do is instead of manually assigning movies
+	# assign a list of movies that is returned by another script (much like get_theaters)
+	
 	theaters_movies[theaters] = ['movie1',
 								 'movie2',
 								 'movie3',
